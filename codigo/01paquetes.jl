d1fcaa1ae6a5ed237c0716f39046eb2f7c60bd34
Arturo Erdély <arturoerdely@gmail.com>
2024-08-11 15:23:33 -0600

    nuevos

--- codigo/01paquetes.jl
+++ codigo/01paquetes.jl
@@ -1,8 +1,8 @@
 #=
 
-    Instala algunos paquetes externos necesarios para realizar
-    inferencia estadística, mediante un ciclo. También podrían
-    instalarse uno por uno desde el modo paquete en la terminal mediante
+    Instala algunos paquetes externos necesarios para el curso de 
+    Estadística II mediante un ciclo. También podrían instalarse
+    uno por uno desde el modo paquete en la terminal mediante
     la instrucción `add` seguida del nombre del paquete.
 
 =#
